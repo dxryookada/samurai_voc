b66efa522f84169cc60df29042cc0e2c679a395c
kazuya-0914 <kazu.work.0914@gmail.com>
2025-01-05 22:13:34 +0900

    1/5 夜 コミット

--- employee_app/views.py
+++ employee_app/views.py
@@ -1,95 +1,129 @@
 from django.utils.timezone import now
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic import TemplateView
-from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import AwardCount
+from django.views import View
+from django.views.generic import TemplateView
+from django.urls import reverse_lazy, reverse
+from .models import CustomUser, AwardCount
+from voc_app.models import AgeGroup
 
-# --- コンテキスト
-class BaseContext:
-    def get_context_data(self, **kwargs):
-        # テンプレートファイルにparamsデータを渡す
-        context = super().get_context_data(**kwargs)
-        params = {
-            'title': '機械学習 | Django基礎',
-            'header': self.header,
-            'current_path': self.request.path, # パスの取得
-            'val': self.val, # 通常の出力
-            'pre': self.pre, # <pre>で出力
-            'url': self.url, # 画像出力
-        }
-        context.update(params)
-        return context
-
-# --- ログインページ
+# --- ログインページ --- #
 class LoginView(LoginView):
-  template_name = "login.html"
-  redirect_authenticated_user = True 
+    template_name = "login.html"
+    redirect_authenticated_user = True 
 
-  def get_success_url(self):
-    if self.request.user.is_staff:
-      return reverse_lazy('admin')
-    return reverse_lazy('general')
+    def get_success_url(self):
+        if self.request.user.is_staff:
+          return reverse_lazy('admin')
+        return reverse_lazy('general')
   
-# --- ログアウト動作
+# --- ログアウト動作 --- #
 class logoutView(LogoutView):
-  def dispatch(self, request, *args, **kwargs):
-    messages.success(request, "ログアウトしました")
-    response = super().dispatch(request, *args, **kwargs)  # 親クラスの処理を呼び出す
-    return response
+    def dispatch(self, request, *args, **kwargs):
+        messages.success(request, "ログアウトしました")
+        response = super().dispatch(request, *args, **kwargs)  # 親クラスの処理を呼び出す
+        return response
 
-# --- 一般従業員ページ（トップ）
+# --- 一般従業員ページ（トップ） --- #
 class GeneralView(LoginRequiredMixin, TemplateView):
     template_name = "general/general.html"
 
-    # コンテキスト
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
+        # ユーザー情報
         user = self.request.user
-        current_year = now().year
-        current_month = now().month
-        years = [current_year - 1, current_year, current_year + 1]
+
+        # 対象年月
+        year = self.request.GET.get('year', now().year) 
+        years = [int(year) - 2, int(year) - 1, int(year)]
+        month = self.request.GET.get('month', now().month)
         months = list(range(1, 13))
 
+        # 表彰 
         award_counts = {
             'gold': AwardCount.objects.filter(employee=user, award_type__name='金').count(),
             'silver': AwardCount.objects.filter(employee=user, award_type__name='銀').count(),
             'copper': AwardCount.objects.filter(employee=user, award_type__name='銅').count(),
         }
+
+        # 年代
+        age_groups = AgeGroup.objects.all()
+
+        # コンテキスト
         context.update({
             'user_name': user.name,
             'advice': user.advice,
             'award_counts': award_counts,
+            'selected_year': int(year),
             'years': years,
-            'current_year': current_year,
+            'selected_month': int(month),
             'months': months,
-            'current_month': current_month,
+            'age_groups': age_groups, 
         })
         return context
 
-# --- 一般従業員ページ（データ更新）
-class UpdateView(LoginRequiredMixin, TemplateView):
-  template_name = "general/update.html"
+# --- 一般従業員ページ（マイページ・データ更新） --- #
+class UpdateView(LoginRequiredMixin, View):
+    template_name = "general/update.html"
 
-# --- 管理者ページ（トップ）
+    # 初期画面
+    def get(self, request, *args, **kwargs):
+        user = get_object_or_404(CustomUser, pk=request.user.pk)
+        context = {
+            'employee_id': user.employee_id,
+            'user_name': user.name,
+            'work_area': user.work_area,
+            'email': user.email,
+        }
+        return render(request, self.template_name, context)
+
+    # データ更新  
+    def post(self, request, *args, **kwargs):
+        employee_id = request.POST.get('id')
+        user_name = request.POST.get('user_name')
+        work_area = request.POST.get('area')
+        email = request.POST.get('email')
+
+        user = get_object_or_404(CustomUser, pk=request.user.pk)
+
+        try:
+            user.employee_id = employee_id
+            user.name = user_name
+            user.work_area = work_area
+            user.email = email
+            user.save()
+
+            messages.success(request, "従業員情報を更新しました")
+            return redirect(reverse("general"))
+        except Exception as e:
+            messages.error(request, "従業員情報の更新に失敗しました")
+            return render(request, self.template_name, {
+                'employee_id': user.employee_id,
+                'user_name': user.name,
+                'work_area': user.work_area,
+                'email': user.email,
+                'error': str(e)
+            })
+
+# --- 管理者ページ（トップ） --- #
 class AdminView(LoginRequiredMixin, TemplateView):
-  template_name = "admin/admin.html"
+    template_name = "admin/admin.html"
 
-# --- お客様の声一覧ページ（管理者）
+# --- お客様の声一覧ページ（管理者） --- #
 class VoicesView(LoginRequiredMixin, TemplateView):
-  template_name = "admin/voices.html"
+    template_name = "admin/voices.html"
 
-# --- 表彰状贈呈一覧ページ（管理者）
+# --- 表彰状贈呈一覧ページ（管理者） --- #
 class AwardsView(LoginRequiredMixin, TemplateView):
-  template_name = "admin/awards.html"
+    template_name = "admin/awards.html"
 
-# --- 従業員一覧ページ（管理者）
+# --- 従業員一覧ページ（管理者） --- #
 class EmployeesView(LoginRequiredMixin, TemplateView):
-  template_name = "admin/employees.html"
+    template_name = "admin/employees.html"
 
-# --- 従業員詳細ページ（管理者）
+# --- 従業員詳細ページ（管理者） --- #
 class DetailView(LoginRequiredMixin, TemplateView):
-  template_name = "admin/detail.html"
+    template_name = "admin/detail.html"
