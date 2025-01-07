9b086b4ad341f63f54cf43df152b8a3558540df8
kazuya-0914 <kazu.work.0914@gmail.com>
2025-01-08 00:43:22 +0900

    1/7 PM コミット

--- voc_app/urls.py
+++ voc_app/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  # --- QRコード発行
   path('', views.PublishView.as_view(), name='publish'),
   path('qr/', views.QrView.as_view(), name='qr'),
+  # --- アンケート
   path('survey/', views.SurveyView.as_view(), name='survey'),
-  path('complete/', views.ComView.as_view(), name='complete'),
+  path('complete/', views.CompleteView.as_view(), name='complete'),
 ]
--- voc_app/views.py
+++ voc_app/views.py
@@ -1,24 +1,60 @@
+import uuid
+import qrcode
+from django.utils.timezone import now
+from datetime import timedelta
+from django.core.files.base import ContentFile
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import TemplateView
+from django.views import View
+from .models import Work
 
-class PublishView(TemplateView):
-  template_name = "survey/publish.html"
+# --- QRコード発行ページ --- #
+class PublishView(View):
+  def get(self, request):
+    return render(request, "survey/publish.html")
 
+  def post(self, request):
+    # フォームデータ取得
+    receipt_number = request.POST.get("receipt-number")
+    receipt_date = request.POST.get("receipt-date")
+    work_start_date = request.POST.get("work-day")
+
+    # 入力データのバリデーション
+    if not receipt_number or not receipt_date or not work_start_date:
+      return render(request, "survey/publish.html", {"error": "すべてのフィールドを入力してください。"})
+    
+    # URLトークン生成と期限設定
+    url_token = str(uuid.uuid4())
+    url_token_expiry = now().date() + timedelta(days=7)  # 7日間有効
+
+    # 工事テーブルに保存
+    work = Work.objects.create(
+      receipt_number=receipt_number,
+      receipt_date=receipt_date,
+      work_start_date=work_start_date,
+      url_token=url_token,
+      url_token_expiry=url_token_expiry,
+    )
+
+    # QRコード生成
+    survey_url = request.build_absolute_uri(reverse('survey', args=[url_token]))
+    qr_image = qrcode.make(survey_url)
+    qr_buffer = ContentFile((b""))
+    qr_image.save(qr_buffer, format="PNG")
+    qr_buffer.seek(0)
+
+    # QRコードをテンプレートで表示
+    return render(request, "survey/qr.html", {"qr_code": qr_buffer, "survey_url": survey_url})
+
+# --- QRコードページ --- #
 class QrView(TemplateView):
   template_name = "survey/qr.html"
 
-  def post(self, request, *args, **kwargs):
-     # POSTリクエストが送信されたときに処理を行いたい場合はここで行います。
-     # 必要な処理を追加してください。 
-     return self.get(request, *args, **kwargs)
-
+# --- アンケートページ --- #
 class SurveyView(TemplateView):
   template_name = "survey/survey.html"
 
-class ComView(TemplateView):
+# --- アンケート送信完了ページ --- #
+class CompleteView(TemplateView):
   template_name = "survey/complete.html"
-
-  def post(self, request, *args, **kwargs):
-    # POSTリクエストが送信されたときに処理を行いたい場合はここで行います。
-    # 必要な処理を追加してください。 
-    return self.get(request, *args, **kwargs)
